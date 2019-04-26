84fdb0f5dc45fee9c3257b80ff3a6c5552a09543
근열 이 <wmf1235@naver.com>
2019-04-26 18:04:07 +0900

    Upload Project File first.

---
+++ CrmSdkLibrary/ConnectionSQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmSdkLibrary
+{
+    class ConnectionSQL
+    {
+        private SqlConnection conn;
+        public SqlConnection Conn
+        {
+            get { return conn;}
+            set { conn = value; }
+        }
+    }
+}
