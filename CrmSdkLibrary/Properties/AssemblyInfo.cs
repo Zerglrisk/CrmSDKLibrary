330c5ed8803c571715141a7e1a7e1209466ec6b5
Zerglrisk <wmf1235@naver.com>
2020-02-25 09:42:36 +0900

    Nuget Creation

--- CrmSdkLibrary/Properties/AssemblyInfo.cs
+++ CrmSdkLibrary/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("CrmSdkLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Crm Sdk Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Zerglrisk")]
 [assembly: AssemblyProduct("CrmSdkLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
