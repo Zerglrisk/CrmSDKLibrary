93200aa9a4933155f5ab4c4357da03d24ad71eba
Zerglrisk <wmf1235@naver.com>
2020-04-21 09:14:17 +0900

    webapi separate from crmsdklibrary, and created as .net standard

---
+++ WebApi/Definition/Model/ApiException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmSdkLibrary.Definition
+{
+    public class ApiExceptionWrapper
+    {
+        public ApiException Error { get; set; }
+    }
+
+    public class ApiException
+    {
+
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public ApiInnerError InnerError { get; set; }
+    }
+
+    public class ApiInnerError : Exception
+    {
+        //public string Message { get; set; }
+        public string Type { get; set; }
+        //public string StackTrace { get; set; }
+
+    }
+}
