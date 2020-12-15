44e1d70c2cc1c4eae1af9cb97cf7ab0173fbcbc1
Zerglrisk <wmf1235@naver.com>
2020-12-15 09:04:20 +0900

    api login test, crm sdk login add,

---
+++ CrmSdkLibrary/Definition/CrmValue/Converters/Value/BooleanConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmSdkLibrary.Definition.CrmValue.Converters.Value
+{
+    internal sealed class BooleanConverter : CrmValueConverter<bool>
+    {
+        public override bool Read(ref CrmValueReader reader, Type typeConvert)
+        {
+            return reader.GetBoolean();
+        }
+
+    }
+}
