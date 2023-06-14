acbe6a239d42cbea8452324f35c31e3f43d44723
Zerglrisk <wmf1235@naver.com>
2023-06-14 10:50:33 +0900

    Fix phenomenon without throwing exceptions in AsyncExtention

--- CrmSdkLibrary/Audits.cs
+++ CrmSdkLibrary/Audits.cs
@@ -5,8 +5,6 @@ using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CrmSdkLibrary
 {
@@ -90,7 +88,7 @@ namespace CrmSdkLibrary
 
         public static bool? GetIsAuditEnabled(IOrganizationService service)
         {
-            //Connection.Service.ConnectedOrgId 
+            //Connection.Service.ConnectedOrgId
             //Connection.Service.OrganizationDetail.OrganizationId
             var orgId = Connection.Service.ConnectedOrgId;
             var org = service.Retrieve("organization", orgId, new Microsoft.Xrm.Sdk.Query.ColumnSet("organizationid", "isauditenabled"));
@@ -101,6 +99,7 @@ namespace CrmSdkLibrary
             }
             return null;
         }
+
         public static AuditDetailCollection GetRecordHistory(IOrganizationService service, string entityLogicalName, Guid recordId)
         {
             var retrieveRecordChangeHistoryRequest = new RetrieveRecordChangeHistoryRequest()
@@ -113,6 +112,7 @@ namespace CrmSdkLibrary
 
             return details;
         }
+
         /// <summary>
         /// Restore Deleted Record using audit guid
         /// </summary>
@@ -136,8 +136,8 @@ namespace CrmSdkLibrary
                     {
                         Entity entity = detail.OldValue;
                         return service.Create(entity);
-                        // The audit records are recorded in the order 
-                        // from latest to older. So, it's ok to break since you've recreated 
+                        // The audit records are recorded in the order
+                        // from latest to older. So, it's ok to break since you've recreated
                         // from the latest snapshot of the entity, just before deletion
                         // break;
                     }
--- CrmSdkLibrary_Core/AsyncExtention.cs
+++ CrmSdkLibrary_Core/AsyncExtention.cs
@@ -1,22 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xrm.Sdk;
+﻿using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
 using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Threading.Tasks;
 
 namespace CrmSdkLibrary_Core
 {
     public static class AsyncExtention
     {
+        public static async Task AssociateAsync(this IOrganizationService service, string entityName, Guid entityId, Relationship relationship, EntityReferenceCollection relatedEntities)
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                service.Associate(entityName, entityId, relationship, relatedEntities);
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+            });
+
+            await t;
+        }
+
+        public static async Task AuthenticateAsync(this ServiceProxy<IOrganizationService> sdk)
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                sdk.Authenticate();
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+            });
+
+            await t;
+        }
+
+        public static async Task<Guid> CreateAsync(this IOrganizationService sdk, Entity entity)
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                var response = sdk.Create(entity);
+                return response;
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+                else { return task.Result; }
+            });
+
+            return await t;
+            //await t;
+            //if (t.IsFaulted) { throw t.Exception.Flatten(); }
+            //return t.Result;
+        }
+
+        public static async Task DeleteAsync(this IOrganizationService sdk, string entityName, Guid id)
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                sdk.Delete(entityName, id);
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+            });
+
+            await t;
+        }
+
+        public static async Task DisassociateAsync(this IOrganizationService sdk, string entityName, Guid entityId, Relationship relationship, EntityReferenceCollection relatedEntities)
+        {
+            var t = Task.Factory.StartNew(() =>
+            {
+                sdk.Disassociate(entityName, entityId, relationship, relatedEntities);
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+            });
+
+            await t;
+        }
+
         public static async Task<T> ExecuteAsync<T>(this IOrganizationService sdk, OrganizationRequest request) where T : OrganizationResponse
         {
             var t = Task.Factory.StartNew(() =>
             {
                 var response = sdk.Execute(request) as T;
                 return response;
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+                else { return task.Result; }
             });
 
             return await t;
@@ -28,6 +99,10 @@ namespace CrmSdkLibrary_Core
             {
                 var response = sdk.Retrieve(entityName, id, columnSet);
                 return response;
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+                else { return task.Result; }
             });
 
             return await t;
@@ -39,50 +114,13 @@ namespace CrmSdkLibrary_Core
             {
                 var response = sdk.RetrieveMultiple(query);
                 return response;
+            }).ContinueWith(task =>
+            {
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
+                else { return task.Result; }
             });
 
             return await t;
-        }
-
-        public static async Task<Guid> CreateAsync(this IOrganizationService sdk, Entity entity)
-        {
-            var t = Task.Factory.StartNew(() =>
-            {
-                var response = sdk.Create(entity);
-                return response;
-            });
-
-            return await t;
-        }
-
-        public static async Task AssociateAsync(this IOrganizationService sdk, string entityName, Guid entityId, Relationship relationship, EntityReferenceCollection relatedEntities)
-        {
-            var t = Task.Factory.StartNew(() =>
-            {
-                sdk.Associate(entityName, entityId, relationship, relatedEntities);
-            });
-
-            await t;
-        }
-
-        public static async Task DeleteAsync(this IOrganizationService sdk, string entityName, Guid id)
-        {
-            var t = Task.Factory.StartNew(() =>
-            {
-                sdk.Delete(entityName, id);
-            });
-
-            await t;
-        }
-
-        public static async Task DisassociateAsync(this IOrganizationService sdk, string entityName, Guid entityId, Relationship relationship, EntityReferenceCollection relatedEntities)
-        {
-            var t = Task.Factory.StartNew(() =>
-            {
-                sdk.Disassociate(entityName, entityId, relationship, relatedEntities);
-            });
-
-            await t;
         }
 
         public static async Task UpdateAsync(this IOrganizationService sdk, Entity entity)
@@ -90,16 +128,9 @@ namespace CrmSdkLibrary_Core
             var t = Task.Factory.StartNew(() =>
             {
                 sdk.Update(entity);
-            });
-
-            await t;
-        }
-
-        public static async Task AuthenticateAsync(this ServiceProxy<IOrganizationService> sdk, Entity entity)
-        {
-            var t = Task.Factory.StartNew(() =>
+            }).ContinueWith(task =>
             {
-                sdk.Authenticate();
+                if (task.IsFaulted) { throw task.Exception.Flatten(); }
             });
 
             await t;
