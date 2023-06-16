6de285c4fe8a5f9ddaf41db01c6336612233a1e5
Zerglrisk <wmf1235@naver.com>
2023-06-16 17:45:55 +0900

    XrmMapper For Core MVC Solution

---
+++ CrmSdkLibrary_Core/Attributes/XrmMapIgnore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrmSdkLibrary_Core.Attributes
+{
+    /// <summary>
+    /// Ignore setted attribute to Mapping or unMapping
+    /// </summary>
+    public class XrmMapIgnore : Attribute
+    {
+    }
+}
